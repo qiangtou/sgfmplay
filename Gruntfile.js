16d2b0cfda2323ba23c5c314ce1d9c7e6a25ec64
qiangtou <qiangtou@gmail.com>
2013-08-07 18:09:20 +0800

    添加一些grunt支持

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,33 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json');
-  });
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			//文件头部输出信息
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+				beautify:{
+					ascii_only:true
+				}
+			},
+			//具体任务配置
+			sgfmplay: {
+				//源文件
+				src: 'jquery.sgfmplay.js',
+				//目标文件
+				dest: 'build/jquery.sgfmplay.js'
+			},
+			mvc: {
+				//源文件
+				src: 'sgfmmvc.js',
+				//目标文件
+				dest: 'build/sgfmmvc.js'
+			}
+		}
+	});
+	 // 加载指定插件任务
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+ 
+    // 默认执行的任务
+    grunt.registerTask('default', ['uglify:sgfmplay','uglify:mvc']);
 };
 
--- jsmin.bat
+++
@@ -1,6 +0,0 @@
-java -jar js.jar --js=sgfmmvc.js  --js_output_file=sgfmmvc.min.js
-java -jar js.jar --js=play_gq.js --js_output_file=play_gq.min.js
-
-copy sgfmmvc.min.js+play_gq.min.js sgfmplay.min.js
-del sgfmmvc.min.js play_gq.min.js
-pause
